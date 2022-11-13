935c73c121af65fc4ad466849577736690a583bb
Lidia Makarchuk <lidiamakarcuk9@gmail.com>
2022-11-14 00:23:40 +0200

    Add a new folder. Add classes to work with files (Page and Record)

---
+++ RedBlackTreeAlgo/DatabaseManager/Page.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBlackTreeAlgo.DatabaseManager
+{
+    public enum Type
+    {
+        index,
+        data
+    }
+    public class Page//internal?
+    {
+        /* Class Page represents the block in file.
+         */
+        public static int pageSize;    //page size in bytes
+        //header
+        private int _number;
+        private Type _type;  //page type, shows which information is stored in this part
+        private int _freeSpace;  //how many bytes are avaliable in this page 
+        private int _position;   //position to add records
+        private bool _isDirty;   //has been currently written or not
+
+        //getters/setters
+
+
+        public Page(int number, Type type, int freeSpace)
+        {
+            this._number = number;
+            this._type = type;
+            this._freeSpace = freeSpace;
+            this._position = sizeof(int)*3+sizeof(Type)+ sizeof(bool);
+            this._isDirty = false;
+        }
+        public Page()
+        {
+            PageDeserialization();
+        }
+        public void PageSerialization()
+        {
+
+        }
+        public void PageDeserialization()
+        {
+
+        }
+    }
+}
---
+++ RedBlackTreeAlgo/DatabaseManager/Record.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBlackTreeAlgo.DatabaseManager
+{
+    public class Record
+    {
+        /*Represents the record in the page. For index type page contains key, index for data of the key (page-row), index for a left successor (page-row) and index for a right successor (page-row)
+         */
+        int key;    //
+        int dataPage;
+        int dataOffset; //number of bytes from the page start
+        int leftPage;
+        int leftOffset;
+        int rightPage;
+        int rightOffset;
+    }
+}
