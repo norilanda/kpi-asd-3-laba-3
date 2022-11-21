9759f53e18ab59f2bd10a38ec22d3ebff899a4fa
Lidia Makarchuk <lidiamakarcuk9@gmail.com>
2022-11-21 19:44:26 +0200

    Add test class

---
+++ RedBlackTreeAlgo/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBlackTreeAlgo
+{
+    public  class Test
+    {
+        public static void createDB10_000Records(string name)
+        {
+
+        }
+    }
+}
